1f8a1e2869c3de7a01410d5a9be53607699f5817
Radzik69 <radzikowski.mateusz@gmail.com>
2026-05-05 01:07:03 +0200

    check_show_media_types filter

--- backend/app/db/get_data/get_recommended_animes_data.py
+++ backend/app/db/get_data/get_recommended_animes_data.py
@@ -2,20 +2,30 @@ import psycopg2
 
 from backend.config.db_settings import HOST,DATABASE,USER,PASSWORD
 
-def get_recommended_animes_data(anime_list):
-    placeholders = ','.join(['%s'] * len(anime_list))
+
+def get_recommended_animes_data(anime_list, media_types="TV"):
+    ANIME_FORMATS = ("TV", "TV_SHORT", "MOVIE", "SPECIAL", "OVA", "ONA", "MUSIC")
+    MANGA_FORMATS = ("MANGA", "NOVEL", "ONE_SHOT")
+
+    formats = MANGA_FORMATS if media_types == "MANGA" else ANIME_FORMATS
+
+    placeholders_titles = ','.join(['%s'] * len(anime_list))
+    placeholders_formats = ','.join(['%s'] * len(formats))
 
     SQL = f"""
-    SELECT id,id_mal,title_english,season_year,format,is_adult,mean_score,description,episode_number,cover_image,trailer_id,trailer_site,season FROM anime_data
-    WHERE title_english IN ({placeholders})
+    SELECT id, id_mal, title_english, season_year, format, is_adult,
+           mean_score, description, episode_number, cover_image,
+           trailer_id, trailer_site, season
+    FROM anime_data
+    WHERE title_english IN ({placeholders_titles})
+    AND format IN ({placeholders_formats})
     """
 
     config = {'host': HOST, 'database': DATABASE, 'user': USER, 'password': PASSWORD}
-
     try:
         with psycopg2.connect(**config) as conn:
             with conn.cursor() as cur:
-                cur.execute(SQL, anime_list)
+                cur.execute(SQL, list(anime_list) + list(formats))
                 return cur.fetchall()
     except Exception as e:
         print("DB ERROR:", e)
